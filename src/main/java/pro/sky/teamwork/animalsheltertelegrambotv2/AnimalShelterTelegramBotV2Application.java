b4b62449e64a0abc372e015201098bf5745f9268
Zhoss <joss.90@mail.ru>
2023-04-05 20:37:37 +1000

    Initial commit

---
+++ src/main/java/pro/sky/teamwork/animalsheltertelegrambotv2/AnimalShelterTelegramBotV2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.teamwork.animalsheltertelegrambotv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalShelterTelegramBotV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalShelterTelegramBotV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/teamwork/animalsheltertelegrambotv2/AnimalShelterTelegramBotV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.teamwork.animalsheltertelegrambotv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterTelegramBotV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
